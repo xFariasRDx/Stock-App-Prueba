eeb55abc67eae20b75484b05fffd3fc20a15f5b1
Miguel Farias <fariasxd1@gmail.com>
2023-12-01 14:20:37 -0400

    First Commit

---
+++ StockApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StockApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
